000d1ef51960cb22a4df1e0d3b38fb30b1c688ac
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2022-07-31 17:20:53 +0530

    species richness graph

--- pmp-yearly-patterns.R
+++ pmp-yearly-patterns.R
@@ -79,6 +79,15 @@ filt_spec <- data_pmp %>%
   slice(1:3) %>% 
   distinct(OBSERVER.ID, LOCALITY.ID, COMMON.NAME)
 
+filt_loc <- data_pmp %>% 
+  group_by(OBSERVER.ID, LOCALITY.ID, SEASON) %>% 
+  summarise(N.MONTHS = n_distinct(MONTH)) %>% 
+  # every month per season
+  filter(N.MONTHS == 3) %>% 
+  summarise(N.SEASONS = n_distinct(SEASON)) %>% 
+  # at least 2 seasons
+  filter(N.SEASONS >= 2) %>% 
+  distinct(OBSERVER.ID, LOCALITY.ID)
 
 ##### map of PMP observations ####
 
@@ -136,16 +145,11 @@ data1 <- data_pmp %>%
             TOT.LISTS = min(TOT.LISTS),
             REP.FREQ = (N.LISTS/TOT.LISTS) * 100)
 
-for (i in 1:n_distinct(data1$OBSERVER.ID)) {
-  
-  data_temp <- filter(data1, OBSERVER.ID == data1$OBSERVER.ID[i])
-  
-  plot_temp <- ggplot(data_temp, 
-                      aes(SEASON, REP.FREQ, group = COMMON.NAME, colour = REP.FREQ)) +
-    geom_point() +
-    facet_wrap(~ LOCALITY)
-  
-}
+# for (i in 1:n_distinct(data1$OBSERVER.ID)) {
+#   
+# 
+#   
+# }
 
 obs_temp <- unique(data1$OBSERVER.ID)[1]
 data_temp <- filter(data1, OBSERVER.ID == obs_temp)
@@ -186,3 +190,70 @@ plot_temp <- ggplot(data_temp,
 ggsave("temp.png", plot_temp, dpi = 300,
        width = 8, height = 3*n_loc, units = "in")
        
+
+##### change in species richness ####
+
+data2 <- data_pmp %>% 
+  group_by(OBSERVER.ID, FULL.NAME, LOCALITY, LOCALITY.ID, 
+           SEASON, SAMPLING.EVENT.IDENTIFIER) %>% 
+  summarise(NO.SP = n_distinct(COMMON.NAME)) %>% 
+  ungroup() %>% 
+  right_join(filt_spec)
+
+obs_temp <- unique(data2$OBSERVER.ID)[1]
+data_temp <- filter(data2, OBSERVER.ID == obs_temp)
+
+n_loc <- n_distinct(data_temp$LOCALITY)
+
+
+median_IQR <- function(x) {
+  data.frame(y = median(x), # Median
+             ymin = quantile(x)[2], # 1st quartile
+             ymax = quantile(x)[4])  # 3rd quartile
+}
+
+# Function for min, max values
+range <- function(x) {
+  data.frame(ymin = min(x),
+             ymax = max(x))
+}
+
+plot_temp <- ggplot(data_temp, 
+                    aes(as.numeric(SEASON), NO.SP)) +
+  facet_wrap(~ LOCALITY, ncol = 1, scales = "free") +
+  # main points
+  stat_summary(geom = "point", fun = "median",
+               size = 5, col = "#1D6F72") + 
+  stat_summary(geom = "linerange", fun.data = median_IQR, 
+               size = 3, col = "#1D6F72", alpha = 0.6) +
+  stat_summary(geom = "linerange", fun.data = range, 
+               size = 3, col = "#1D6F72", alpha = 0.3) +
+  # data points
+  geom_point(size = 3, alpha = 0.05, position = position_jitter(width = 0.1)) +
+  scale_x_continuous(labels = unique(data_temp$SEASON)) +
+  scale_y_continuous(breaks = seq(0, 100, 5)) +
+  labs(title = glue("{data_temp$FULL.NAME}'s patches"),
+       subtitle = glue("\n \n{data_annotation} \n \n"),
+       x = "Season", y = "Number of species reported") +
+  theme(axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_line(linetype = 3, colour = "#C2C2C2"),
+        plot.title = element_text(hjust = -0.1, size = 16),
+        plot.subtitle = element_text(hjust = -0.09, size = 8, 
+                                     colour = "#ADADAD", face = "italic"),
+        strip.text = element_text(size = 10),
+        strip.background = element_rect(fill = "#D6D6D6", colour = NA),
+        plot.background = element_rect(fill = "#EAEAEB", colour = NA),
+        panel.background = element_rect(fill = "#EAEAEB", colour = NA),
+        legend.background = element_blank(),
+        legend.text = element_text(size = 7),
+        # no fill around legend points
+        legend.key = element_blank(), 
+        panel.spacing = unit(2, "lines"),
+        plot.margin = unit(c(1, 0.5, 1, 0.5), "lines"))
+
+ggsave("temp.png", plot_temp, dpi = 300,
+       width = 6, height = 3*n_loc, units = "in")
+
+
