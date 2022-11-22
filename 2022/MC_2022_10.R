39e4a028574c46ef14c96fe42a0edcb336668f09
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2022-11-22 14:14:14 +0530

    monthly challenge for Oct 2022 first commit

---
+++ 2022/MC_2022_10.R
@@ -0,0 +1,38 @@
+
+###### monthly challenge winners/results ###
+
+# basic eligible list filter
+data0 <- data_mc %>% 
+  filter(ALL.SPECIES.REPORTED == 1, DURATION.MINUTES >= 14) %>% 
+  group_by(SAMPLING.EVENT.IDENTIFIER) %>% 
+  filter(!any(OBSERVATION.COUNT == "X")) %>% 
+  ungroup()
+
+# at least 40 lists
+data1 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 40)
+
+# at least 5 lists on Oct 8
+data2 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  filter(DAY.M == 8) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 5)
+
+
+results <- data1 %>% 
+  inner_join(data2) %>% 
+  left_join(eBird_users) %>% 
+  anti_join(filtGA)
+
+
+
+# random selection 
+a <- results %>% 
+  filter(FULL.NAME != "MetalClicks Ajay Ashok") # removes NAs too
+set.seed(20)
+winner <- a %>% slice_sample(n = 1) %>% select(FULL.NAME)
+glue("Monthly challenge winner is {winner}")
+
