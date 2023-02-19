a91301523057d776db40b1bb72b3f6f69c8f8db4
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2023-02-19 14:19:45 +0530

    adding locations to basic stats, plus parameter to determine how to format output

--- summaries.R
+++ summaries.R
@@ -3,7 +3,7 @@
 
 # data needs to be filtered and processed with column GROUP.ID
 
-basic_stats <- function(data) {
+basic_stats <- function(data, prettify = T) {
   
   require(tidyverse)
   require(magrittr)
@@ -27,22 +27,44 @@ basic_stats <- function(data) {
     ungroup() %$%
     n_distinct(GROUP.ID)
   
+  
   tot_specs <- data %>% 
     filter(CATEGORY %in% c("species","issf")) %$% 
     n_distinct(COMMON.NAME)
   
+  tot_locs <- n_distinct(data$LOCALITY.ID)
   
-  stats <- data.frame(A = tot_bdr, 
-                      B = tot_obs,
-                      C = tot_lists,
-                      D = tot_ulists,
-                      E = tot_clists,
-                      F = tot_mlists,
-                      G = tot_specs) %>% 
-    magrittr::set_colnames(c("eBirders", "observations", "lists (all types)", 
-                             "unique lists", "complete lists", "unique lists with media",
-                             "species")) %>% 
-    pivot_longer(everything(), names_to = "Number of", values_to = "Values")
+  
+  if (prettify == T) {
+    
+    stats <- data.frame(A = tot_bdr, 
+                        B = tot_obs,
+                        C = tot_lists,
+                        D = tot_ulists,
+                        E = tot_clists,
+                        F = tot_mlists,
+                        G = tot_specs,
+                        H = tot_locs) %>% 
+      magrittr::set_colnames(c("eBirders", "observations", "lists (all types)", 
+                               "unique lists", "complete lists", "unique lists with media",
+                               "species", "locations")) %>% 
+      pivot_longer(everything(), names_to = "Number of", values_to = "Values")
+    
+  } else if (prettify == F) {
+    
+    stats <- data.frame(A = tot_bdr, 
+                        B = tot_obs,
+                        C = tot_lists,
+                        D = tot_ulists,
+                        E = tot_clists,
+                        F = tot_mlists,
+                        G = tot_specs,
+                        H = tot_locs) %>% 
+      magrittr::set_colnames(c("PARTICIPANTS", "OBSERVATIONS", "LISTS.ALL", "LISTS.U", 
+                               "LISTS.C", "LISTS.M", "SPECIES", "LOCATIONS"))
+    
+  }
+  
   
   return(stats)
   
